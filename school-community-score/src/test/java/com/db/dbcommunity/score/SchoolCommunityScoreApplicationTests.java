84750ddb0c0c7bfff9fd441257ac67c02c23f7a2
logeek <2031500185@qq.com>
2023-02-08 17:22:50 +0800

    初始化评分服务

---
+++ school-community-score/src/test/java/com/db/dbcommunity/score/SchoolCommunityScoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.db.dbcommunity.score;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolCommunityScoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
