8e7ffc68b3799599bd5c98a39cc4a52bbdb36b55
logeek <2031500185@qq.com>
2023-02-08 16:10:04 +0800

    初始化访问服务

---
+++ school-community-visit/src/main/java/com/db/dbcommunity/visit/VisitApplication.java
@@ -0,0 +1,15 @@
+package com.db.dbcommunity.visit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class VisitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VisitApplication.class, args);
+	}
+
+}
---
+++ school-community-visit/src/test/java/com/db/dbcommunity/visit/SchoolCommunityVisitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.db.dbcommunity.visit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolCommunityVisitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
