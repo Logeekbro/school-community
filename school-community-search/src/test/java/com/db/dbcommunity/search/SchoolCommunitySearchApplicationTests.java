e14bed7bfb88266a051dac0eeedd037a85cda522
logeek <2031500185@qq.com>
2023-02-08 16:03:01 +0800

    初始化搜索服务

---
+++ school-community-search/src/test/java/com/db/dbcommunity/search/SchoolCommunitySearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.db.dbcommunity.search;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolCommunitySearchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
