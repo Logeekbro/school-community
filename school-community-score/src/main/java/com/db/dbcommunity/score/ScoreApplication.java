ed730bd47e02e6bdfe5d519e036df185a446a5e4
logeek <2031500185@qq.com>
2023-02-08 16:14:32 +0800

    初始化评分服务

---
+++ school-community-score/src/main/java/com/db/dbcommunity/score/ScoreApplication.java
@@ -0,0 +1,15 @@
+package com.db.dbcommunity.score;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ScoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScoreApplication.class, args);
+    }
+
+}
