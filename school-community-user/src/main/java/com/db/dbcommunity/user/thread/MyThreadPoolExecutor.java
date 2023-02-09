7ab348613824884a0572046d24ba1d8d94896744
logeek <2031500185@qq.com>
2023-02-09 22:08:31 +0800

    添加了自定义的线程池，用于执行异步任务

---
+++ school-community-user/src/main/java/com/db/dbcommunity/user/thread/MyThreadPoolExecutor.java
@@ -0,0 +1,23 @@
+package com.db.dbcommunity.user.thread;
+
+import java.util.concurrent.*;
+
+public class MyThreadPoolExecutor {
+
+    private static final ExecutorService threadPool;
+
+    static {
+        threadPool = new ThreadPoolExecutor(
+                10, 20, 128, TimeUnit.SECONDS,
+                new LinkedBlockingDeque<>(100),
+                Executors.defaultThreadFactory(),
+                new ThreadPoolExecutor.AbortPolicy());
+    }
+
+    public static void run(Runnable runnable) {
+        CompletableFuture.runAsync(runnable, threadPool);
+    }
+
+
+
+}
