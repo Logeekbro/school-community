3348fe66d45937c4d005717af64a0d555d22bccf
logeek <2031500185@qq.com>
2023-02-09 22:07:09 +0800

    新增一些常量

---
+++ school-community-common/common-base/src/main/java/com/db/dbcommunity/common/constant/MessageConstant.java
@@ -0,0 +1,14 @@
+package com.db.dbcommunity.common.constant;
+
+public interface MessageConstant {
+
+    /**
+     * 消息服务的路径前缀
+     */
+    String URI_PREFIX = "/message";
+
+    /**
+     * 发送系统消息时使用的id
+     */
+    Long SYSTEM_ID = -1L;
+}
--- school-community-common/common-base/src/main/java/com/db/dbcommunity/common/constant/UserConstant.java
+++ school-community-common/common-base/src/main/java/com/db/dbcommunity/common/constant/UserConstant.java
@@ -3,4 +3,7 @@ package com.db.dbcommunity.common.constant;
 public interface UserConstant {
     // 用户服务的访问路径前缀
     String URI_PREFIX = "/user";
+
+    // 普通用户的角色id
+    Integer DEFAULT_USER_ROLE_ID = 10;
 }
