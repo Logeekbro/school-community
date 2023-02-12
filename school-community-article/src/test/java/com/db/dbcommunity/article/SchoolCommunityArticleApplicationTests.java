988bfe9cfd11003bab312a77661b327f17ed084e
logeek <2031500185@qq.com>
2023-02-12 17:22:58 +0800

    测试类

--- school-community-article/src/test/java/com/db/dbcommunity/article/SchoolCommunityArticleApplicationTests.java
+++ school-community-article/src/test/java/com/db/dbcommunity/article/SchoolCommunityArticleApplicationTests.java
@@ -1,13 +1,30 @@
 package com.db.dbcommunity.article;
 
+import com.db.dbcommunity.article.service.TagService;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.annotation.Resource;
+import java.util.HashSet;
+import java.util.Set;
 
 @SpringBootTest
 class SchoolCommunityArticleApplicationTests {
 
+    @Resource
+    private TagService tagService;
+
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void TestTagsCreate() {
+        Set<String> set = new HashSet<>();
+        set.add("111");
+        set.add("222");
+        set.add("111");
+        tagService.handleArticleTagCreate(1L, set);
     }
 
 }
