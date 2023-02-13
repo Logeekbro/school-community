7612f7f74678ae06ce5f0c71b66202ac0104270c
logeek <2031500185@qq.com>
2023-02-13 22:08:20 +0800

    添加了事务支持

--- school-community-user/src/main/java/com/db/dbcommunity/user/UserApplication.java
+++ school-community-user/src/main/java/com/db/dbcommunity/user/UserApplication.java
@@ -4,7 +4,9 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+@EnableTransactionManagement
 @SpringBootApplication(scanBasePackages = {"com.db.dbcommunity.*"})
 @EnableDiscoveryClient
 // 此处一定要指定具体路径，否则会扫到奇怪的东西...然后报错
