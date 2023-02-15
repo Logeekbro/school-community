dc8f7fa6979791e4415bf75793af83bbaa52997c
logeek <2031500185@qq.com>
2023-02-15 20:12:48 +0800

    为同一线程内的不同方法之间通过非形参方式传递参数提供支持

---
+++ school-community-article/src/main/java/com/db/dbcommunity/article/thread/ServiceContext.java
@@ -0,0 +1,29 @@
+package com.db.dbcommunity.article.thread;
+
+import com.db.dbcommunity.article.model.vo.ArticleReviewResultVO;
+
+public class ServiceContext {
+
+    private static final ThreadLocal<Long> articleId = new ThreadLocal<>();
+
+    private static final ThreadLocal<ArticleReviewResultVO> reviewResultVo = new ThreadLocal<>();
+
+    public static void setArticleId(Long id) {
+        articleId.set(id);
+    }
+
+    public static Long getArticleId() {
+        return articleId.get();
+    }
+
+    public static void setReviewResultVo(ArticleReviewResultVO vo) {
+        reviewResultVo.set(vo);
+    }
+
+    public static ArticleReviewResultVO getReviewResultVo() {
+        return reviewResultVo.get();
+    }
+
+
+
+}
