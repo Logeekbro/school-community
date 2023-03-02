7ea4fd8a80e1a6e628e5c277523e4432bc073a61
logeek <2031500185@qq.com>
2023-03-02 17:52:35 +0800

    删除无用导包

--- school-community-relation/src/main/java/com/db/dbcommunity/relation/service/impl/FollowServiceImpl.java
+++ school-community-relation/src/main/java/com/db/dbcommunity/relation/service/impl/FollowServiceImpl.java
@@ -7,8 +7,6 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
-import java.util.List;
-import java.util.Set;
 
 @Service
 public class FollowServiceImpl implements FollowService {
