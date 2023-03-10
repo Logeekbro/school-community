add93933b39b52d9cd0aed6be8d6de01f714535a
logeek <2031500185@qq.com>
2023-03-10 10:13:50 +0800

    开启缓存

--- school-community-article/src/main/java/com/db/dbcommunity/article/ArticleApplication.java
+++ school-community-article/src/main/java/com/db/dbcommunity/article/ArticleApplication.java
@@ -3,12 +3,14 @@ package com.db.dbcommunity.article;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication(scanBasePackages = {"com.db.dbcommunity.*"})
 @EnableDiscoveryClient
 // 此处一定要指定具体路径，否则会扫到奇怪的东西...然后报错
 @MapperScan(basePackages = {"com.db.dbcommunity.*.mapper"})
+@EnableCaching
 public class ArticleApplication {
 
     public static void main(String[] args) {
