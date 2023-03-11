f634f251b71ff598a3334e93001a69ddbf6eea44
logeek <2031500185@qq.com>
2023-03-11 18:03:47 +0800

    存入ES的文章对象

---
+++ school-community-search/src/main/java/com/db/dbcommunity/search/document/Article.java
@@ -0,0 +1,13 @@
+package com.db.dbcommunity.search.document;
+
+import lombok.Data;
+
+@Data
+public class Article {
+
+    private Long articleId;
+
+    private String title;
+
+    private String summary;
+}
