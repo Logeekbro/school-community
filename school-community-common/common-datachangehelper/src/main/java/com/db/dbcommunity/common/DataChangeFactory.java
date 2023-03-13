9e1550cac4592cd85a7ddb4e8fa009b036e4335b
logeek <2031500185@qq.com>
2023-03-13 11:16:42 +0800

    提供单个dataChangeType绑定多个handler支持

--- school-community-common/common-datachangehelper/src/main/java/com/db/dbcommunity/common/DataChangeFactory.java
+++ school-community-common/common-datachangehelper/src/main/java/com/db/dbcommunity/common/DataChangeFactory.java
@@ -3,18 +3,22 @@ package com.db.dbcommunity.common;
 import com.sun.istack.internal.logging.Logger;
 
 import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 
 public class DataChangeFactory {
 
     private static final Logger logger = Logger.getLogger(DataChangeFactory.class);
 
-    private static final Map<String, Runnable> handlerMap = new HashMap<>();
+    private static final Map<String, List<Runnable>> handlerMap = new HashMap<>();
 
     public static void handle(String type) {
-        Runnable runnable = handlerMap.get(type);
-        if(runnable != null) {
-            runnable.run();
+        List<Runnable> tasks = handlerMap.get(type);
+        if(tasks != null) {
+            for(Runnable task : tasks) {
+                task.run();
+            }
         } else {
             logger.warning("not handler provided for this type: " + type);
         }
@@ -22,7 +26,10 @@ public class DataChangeFactory {
 
     public static void addHandler(IDataChangeHandler handler) {
         for (String type : handler.handleType()) {
-            handlerMap.put(type, handler.runnable());
+            List<Runnable> tasks = handlerMap.get(type);
+            if(tasks == null) tasks = new LinkedList<>();
+            else tasks.add(handler.runnable());
+            handlerMap.put(type, tasks);
         }
 
     }
