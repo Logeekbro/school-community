cf85c21719f37d2554fa176b99bef9fe2f898106
logeek <2031500185@qq.com>
2023-03-13 09:42:54 +0800

    启用openFeign

--- school-community-article/src/main/java/com/db/dbcommunity/article/ArticleApplication.java
+++ school-community-article/src/main/java/com/db/dbcommunity/article/ArticleApplication.java
@@ -5,11 +5,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication(scanBasePackages = {"com.db.dbcommunity.*"})
 @EnableDiscoveryClient
 // 此处一定要指定具体路径，否则会扫到奇怪的东西...然后报错
 @MapperScan(basePackages = {"com.db.dbcommunity.*.mapper"})
+@EnableFeignClients(basePackages = "com.db.dbcommunity.article.feign")
 @EnableCaching
 public class ArticleApplication {
 
