d81b4bbb8a5908b3abb827be7d91daf0f7fdb685
logeek <2031500185@qq.com>
2023-03-13 12:40:48 +0800

    -

--- school-community-common/common-mysql/src/main/java/com/db/dbcommunity/common/model/entity/BaseEntity.java
+++ school-community-common/common-mysql/src/main/java/com/db/dbcommunity/common/model/entity/BaseEntity.java
@@ -3,12 +3,12 @@ package com.db.dbcommunity.common.model.entity;
 import com.baomidou.mybatisplus.annotation.FieldFill;
 import com.baomidou.mybatisplus.annotation.TableField;
 import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.util.Date;
 
 @Data
 public class BaseEntity {
-
     @TableField(value = "create_time",fill = FieldFill.INSERT)
     private Date createTime;
     @TableField(value = "update_time",fill = FieldFill.INSERT)
