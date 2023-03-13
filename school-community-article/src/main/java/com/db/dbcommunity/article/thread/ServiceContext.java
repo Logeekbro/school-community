508e0bd37f88f9c5003f9ba9c6d4c52f23e89bdd
logeek <2031500185@qq.com>
2023-03-13 18:58:41 +0800

    新增article传递支持

--- school-community-article/src/main/java/com/db/dbcommunity/article/thread/ServiceContext.java
+++ school-community-article/src/main/java/com/db/dbcommunity/article/thread/ServiceContext.java
@@ -1,5 +1,6 @@
 package com.db.dbcommunity.article.thread;
 
+import com.db.dbcommunity.article.model.entity.Article;
 import com.db.dbcommunity.article.model.vo.ArticleReviewResultVO;
 
 public class ServiceContext {
@@ -7,6 +8,8 @@ public class ServiceContext {
     private static final ThreadLocal<Long> articleId = new ThreadLocal<>();
 
     private static final ThreadLocal<ArticleReviewResultVO> reviewResultVo = new ThreadLocal<>();
+
+    private static final ThreadLocal<Article> article = new ThreadLocal<>();
 
     public static void setArticleId(Long id) {
         articleId.set(id);
@@ -22,6 +25,14 @@ public class ServiceContext {
 
     public static ArticleReviewResultVO getReviewResultVo() {
         return reviewResultVo.get();
+    }
+
+    public static Article getArticle() {
+        return article.get();
+    }
+
+    public static void setArticle(Article a) {
+        article.set(a);
     }
 
 }
