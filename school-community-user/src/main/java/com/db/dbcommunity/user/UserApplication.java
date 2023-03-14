06d7334a8aefc40e914996040eb6b19f50df340a
logeek <2031500185@qq.com>
2023-03-14 18:14:55 +0800

    开启feignClient

--- school-community-user/src/main/java/com/db/dbcommunity/user/UserApplication.java
+++ school-community-user/src/main/java/com/db/dbcommunity/user/UserApplication.java
@@ -4,6 +4,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @EnableTransactionManagement
@@ -11,6 +12,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableDiscoveryClient
 // 此处一定要指定具体路径，否则会扫到奇怪的东西...然后报错
 @MapperScan(basePackages = {"com.db.dbcommunity.*.mapper"})
+@EnableFeignClients
 public class UserApplication {
 
     public static void main(String[] args) {
