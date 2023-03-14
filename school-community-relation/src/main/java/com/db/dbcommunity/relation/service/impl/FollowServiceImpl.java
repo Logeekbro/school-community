fc9071030b62a2632b0dc16b9c38e3657bf0d80d
logeek <2031500185@qq.com>
2023-03-14 17:01:36 +0800

    改个变量名，防止注入失败

--- school-community-relation/src/main/java/com/db/dbcommunity/relation/service/impl/FollowServiceImpl.java
+++ school-community-relation/src/main/java/com/db/dbcommunity/relation/service/impl/FollowServiceImpl.java
@@ -3,7 +3,6 @@ package com.db.dbcommunity.relation.service.impl;
 import com.db.dbcommunity.common.constant.RedisNameSpace;
 import com.db.dbcommunity.common.util.MyPage;
 import com.db.dbcommunity.relation.service.FollowService;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
@@ -13,39 +12,39 @@ import javax.annotation.Resource;
 public class FollowServiceImpl implements FollowService {
 
     @Resource
-    private StringRedisTemplate redisTemplate;
+    private StringRedisTemplate stringRedisTemplate;
 
 
     @Override
     public boolean changeFollow(Long currentUserId, Long beFollowUserId, boolean isFollow) {
         if(isFollow) {
             long now = System.currentTimeMillis();
-            redisTemplate.opsForZSet()
+            stringRedisTemplate.opsForZSet()
                     .add(RedisNameSpace.FOLLOW_PREFIX + currentUserId, beFollowUserId.toString(), now);
-            redisTemplate.opsForZSet()
+            stringRedisTemplate.opsForZSet()
                     .add(RedisNameSpace.FANS_PREFIX + beFollowUserId, currentUserId.toString(), now);
         } else {
-            redisTemplate.opsForZSet()
+            stringRedisTemplate.opsForZSet()
                     .remove(RedisNameSpace.FOLLOW_PREFIX + currentUserId, beFollowUserId);
-            redisTemplate.opsForZSet()
+            stringRedisTemplate.opsForZSet()
                     .remove(RedisNameSpace.FANS_PREFIX + beFollowUserId, currentUserId);
         }
         // 将当前被关注者id存入粉丝数量有更新的集合中
-        redisTemplate.opsForSet().add(RedisNameSpace.FANS_COUNT_BE_UPDATED_USER_IDS, beFollowUserId.toString());
+        stringRedisTemplate.opsForSet().add(RedisNameSpace.FANS_COUNT_BE_UPDATED_USER_IDS, beFollowUserId.toString());
         return true;
     }
 
     @Override
     public boolean isFollow(Long currentUserId, Long beFollowUserId) {
-        return redisTemplate.opsForZSet().rank(RedisNameSpace.FOLLOW_PREFIX + currentUserId, beFollowUserId) != null;
+        return stringRedisTemplate.opsForZSet().rank(RedisNameSpace.FOLLOW_PREFIX + currentUserId, beFollowUserId) != null;
     }
 
     @Override
     public MyPage<String> getFollowList(Long currentUserId, Long current, Short size) {
         String key = RedisNameSpace.FOLLOW_PREFIX + currentUserId;
-        Long total = redisTemplate.opsForZSet().size(key);
+        Long total = stringRedisTemplate.opsForZSet().size(key);
         MyPage<String> myPage = new MyPage<>(current, size, total);
-        myPage.setRecords(redisTemplate.opsForZSet().range(key, myPage.offset(), myPage.offset() + size - 1));
+        myPage.setRecords(stringRedisTemplate.opsForZSet().range(key, myPage.offset(), myPage.offset() + size - 1));
         return myPage;
     }
 
