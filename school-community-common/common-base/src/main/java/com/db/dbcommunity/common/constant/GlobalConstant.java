5af767b2fa5a93ed4007b80963d2e23da23ecc84
logeek <2031500185@qq.com>
2023-03-16 17:04:23 +0800

    调整：key名称调整

--- school-community-common/common-base/src/main/java/com/db/dbcommunity/common/constant/GlobalConstant.java
+++ school-community-common/common-base/src/main/java/com/db/dbcommunity/common/constant/GlobalConstant.java
@@ -1,13 +1,11 @@
 package com.db.dbcommunity.common.constant;
 
-import sun.awt.HKSCS;
-
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
 public interface GlobalConstant {
-    String URL_PERM_ROLES_KEY = "system:perm_roles_rule:url:";
+    String ROLE_URL_PERMS_KEY = "system:role:permUrls:";
     Integer STATUS_ON = 0;
     Integer STATUS_OFF = 1;
     String USER_DEFAULT_PASSWORD = "123456";
