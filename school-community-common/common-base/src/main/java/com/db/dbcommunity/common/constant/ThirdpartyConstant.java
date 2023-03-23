66c5e793c9bf4a9949ff9631739f65e883857be5
logeek <2031500185@qq.com>
2023-03-23 10:56:23 +0800

    新增常量

--- school-community-common/common-base/src/main/java/com/db/dbcommunity/common/constant/ThirdpartyConstant.java
+++ school-community-common/common-base/src/main/java/com/db/dbcommunity/common/constant/ThirdpartyConstant.java
@@ -8,4 +8,5 @@ public interface ThirdpartyConstant {
     String OSS_FILE_PREFIX = "http://db-community.oss-cn-guangzhou.aliyuncs.com/";
     String OSS_ENDPOINT = "oss-cn-guangzhou.aliyuncs.com";
     String AVATAR_URL_PREFIX = "avatar/";
+    String ARTICLE_IMG_URL_PREFIX = "article/";
 }
