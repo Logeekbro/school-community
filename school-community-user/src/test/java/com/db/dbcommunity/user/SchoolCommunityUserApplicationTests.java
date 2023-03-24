188033cd25d27f220c6695861b244ab7b02a5b04
logeek <2031500185@qq.com>
2023-03-24 12:57:15 +0800

    添加了自动生成权限信息的方法

--- school-community-user/src/test/java/com/db/dbcommunity/user/SchoolCommunityUserApplicationTests.java
+++ school-community-user/src/test/java/com/db/dbcommunity/user/SchoolCommunityUserApplicationTests.java
@@ -1,12 +1,24 @@
 package com.db.dbcommunity.user;
 
+import com.db.dbcommunity.user.controller.UserPermissionController;
+import com.db.dbcommunity.user.controller.UserRoleController;
 import com.db.dbcommunity.user.mapper.RoleMapper;
 import com.db.dbcommunity.user.model.entity.Role;
+import com.db.dbcommunity.user.model.vo.PermissionCreateVO;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.handler.AbstractHandlerMethodMapping;
+import org.springframework.web.servlet.mvc.condition.PathPatternsRequestCondition;
+import org.springframework.web.servlet.mvc.method.RequestMappingInfo;
+import org.springframework.web.util.pattern.PathPattern;
 
 import javax.annotation.Resource;
+import java.util.Map;
+import java.util.Set;
 
 @SpringBootTest
 class SchoolCommunityUserApplicationTests {
@@ -14,8 +26,39 @@ class SchoolCommunityUserApplicationTests {
     @Resource
     private RedisTemplate redisTemplate;
 
+    @Resource
+    private ApplicationContext applicationContext;
+
     @Test
-    void contextLoads() {
+    void generatePermission() {
+        AbstractHandlerMethodMapping<RequestMappingInfo> objHandlerMethodMapping = (AbstractHandlerMethodMapping<RequestMappingInfo>)applicationContext.getBean("requestMappingHandlerMapping");
+        Map<RequestMappingInfo, HandlerMethod> mapRet = objHandlerMethodMapping.getHandlerMethods();
+        for(RequestMappingInfo r : mapRet.keySet()) {
+            if(r.getName() == null) continue;
+            boolean isUserDefault;
+            String name = r.getName();
+            String[] split = name.split("_");
+            if(split.length == 2) {
+                isUserDefault = split[1].equals("true");
+            } else {
+                System.out.println("名称格式有误--->" + name);
+                continue;
+            }
+            Set<RequestMethod> methods = r.getMethodsCondition().getMethods();
+            for(RequestMethod method : methods) {
+                PathPatternsRequestCondition patternsCondition = r.getPathPatternsCondition();
+                if(patternsCondition == null) continue;
+                for(PathPattern path : patternsCondition.getPatterns()) {
+                    String ps = path.getPatternString().replaceAll("\\{.*?}", "*");
+                    String urlPerm = method + ":" + ps;
+                    PermissionCreateVO permissionCreateVO = new PermissionCreateVO();
+                    permissionCreateVO.setIsUserDefault(isUserDefault);
+                    permissionCreateVO.setName(name.substring(0, name.indexOf('_')));
+                    permissionCreateVO.setUrlPerm(urlPerm);
+                    System.out.println(permissionCreateVO);
+                }
+            }
+        }
     }
 
     @Test
--- school-community-visit/src/test/java/com/db/dbcommunity/visit/SchoolCommunityVisitApplicationTests.java
+++ school-community-visit/src/test/java/com/db/dbcommunity/visit/SchoolCommunityVisitApplicationTests.java
@@ -1,13 +1,61 @@
 package com.db.dbcommunity.visit;
 
+import com.db.dbcommunity.visit.feign.UserPermissionFeignClient;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.handler.AbstractHandlerMethodMapping;
+import org.springframework.web.servlet.mvc.condition.PathPatternsRequestCondition;
+import org.springframework.web.servlet.mvc.method.RequestMappingInfo;
+import org.springframework.web.util.pattern.PathPattern;
+
+import javax.annotation.Resource;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
 @SpringBootTest
 class SchoolCommunityVisitApplicationTests {
 
+	@Resource
+	private ApplicationContext applicationContext;
+
+	@Resource
+	private UserPermissionFeignClient userPermissionFeignClient;
+
 	@Test
 	void contextLoads() {
+		AbstractHandlerMethodMapping<RequestMappingInfo> objHandlerMethodMapping = (AbstractHandlerMethodMapping<RequestMappingInfo>)applicationContext.getBean("requestMappingHandlerMapping");
+		Map<RequestMappingInfo, HandlerMethod> mapRet = objHandlerMethodMapping.getHandlerMethods();
+		for(RequestMappingInfo r : mapRet.keySet()) {
+			if(r.getName() == null) continue;
+			boolean isUserDefault;
+			String name = r.getName();
+			String[] split = name.split("_");
+			if(split.length == 2) {
+				isUserDefault = split[1].equals("true");
+			} else {
+				System.out.println("名称格式有误--->" + name);
+				continue;
+			}
+			Set<RequestMethod> methods = r.getMethodsCondition().getMethods();
+			for(RequestMethod method : methods) {
+				PathPatternsRequestCondition patternsCondition = r.getPathPatternsCondition();
+				if(patternsCondition == null) continue;
+				for(PathPattern path : patternsCondition.getPatterns()) {
+					String ps = path.getPatternString().replaceAll("\\{.*?}", "*");
+					String urlPerm = method + ":" + ps;
+					Map<String, Object> map = new HashMap<>();
+					map.put("isUserDefault",isUserDefault);
+					map.put("name",name.substring(0, name.indexOf('_')));
+					map.put("urlPerm",urlPerm);
+					System.out.println(map);
+//					userPermissionFeignClient.addPermission(map);
+				}
+			}
+		}
 	}
 
 }
